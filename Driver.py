fe992a9bb2dbb7d23e35c541affe7b76a9dae473
Blaze Sanders <blaze.d.a.sanders@gmail.com>
2021-06-14 16:39:24 -0700

    WIP: 1st try using pip3 install python-statemachine

--- Driver.py
+++ Driver.py
@@ -4,7 +4,7 @@ __author__  =  "Blaze Sanders"
 __email__   =  "blaze.d.a.sanders@gmail.com"
 __company__ =  "Unlimited Custom Creations"
 __status__  =  "Development"
-__date__    =  "Late Updated: 2021-06-13"
+__date__    =  "Late Updated: 2021-06-14"
 __doc__     =  "Logic to run back-end services via state changes in GUI"
 """
 
@@ -35,6 +35,8 @@ import pyautogui
 # https://docs.python.org/3/library/tempfile.html
 from tempfile import gettempdir
 
+# Allow Driver.py to follow the hardware state machine based off GUI process flow
+from LimonadaStateMachine import *
 
 # Relay Shied Pin CONSTANTS
 SHIELD_PIN_1 = 7
@@ -65,143 +67,190 @@ THIS_CODES_FILENAME = os.path.basename(__file__)
 # Zoom Vidoe & Audio CONSTANTS
 MISSION_CONTROL_ZOOM_URL = ""
 
+class Driver(object):
 
-def start():
-	"""
-	Hardware back-end driver that launches GUI front-end
-	#TODO @Murali - OF DOES GUI front-end driver call into back-end?
-	"""
+	def __init__(self, state):
+		self.state = state
+
+	def unitTest():
+		print("Starting Driver.py Unit Test")
+
+
+	def launchGUI(framework):
+		"""
+    	TODO
+
+    	Key arguments:
+   		framework -- INTERGER: Global CONSTANT for GUI framework this code should launch in parallel
+
+    	Return:
+    	OK if GUI was launched succesfully, and NOT_OK otherwise
+		"""
+
+		if(framework == FLASK):
+			Debug.Dprint("GUI generated using Flask microframework")
+			#TODO Launch GUI.py on 2nd Pi or 2nd thread
+			return OK
+		elif(framework == QTPY):
+			Debug.Dprint("GUI generated using QTPY framework")
+			#TODO Launch GUI.py on 2nd Pi or 2nd thread
+			return OK
+		else:
+			Debug.Dprint("LIKE A DIFFERENT GUI BUILDER? SUBMIT PULL REQUEST")
+
+		return NOT_OK
+
+
+	def vend(productID):
+		"""
+    	#TODO
+
+    	Key argument(s):
+   		productID -- INTERGER: Unique ID for each type on Limonada can flavor
+
+    	Return:
+    	True if ALL the steps to vend a can where successfull; False otherwise
+		"""
+		successfulVend = False
+
+		if(productID == CANNED_DRINK):
+			vendPin = SHIELD_PIN_1
+		elif(productID == GIFT_CARD_IN_CAN):
+			vendPin = SHIELD_PIN_2
+		else:
+			DebugObject = Debug(DEBUG_STATEMENTS_ON, THIS_CODES_FILENAME)
+			DebugObject.Dprint("INVALID productID passed as parameter to Driver.vend()")
+
+		GPIO.setup(vendPin, GPIO.OUT, initial=GPIO.LOW)
+		GPIO.output(vendPin, HIGH)
+		time.pause(VEND_DELAY_IN_SEC)
+		GPIO.output(vendPin, LOW)
+
+		GPIO.setup(sensePin, GPIO.out, initial=GPIO.LOW)
+		itemSensed = GPIO.input(sensePin)
+		time.pause(SENSE_DELAY_IN_SEC)
+
+		if(itemSensed == HIGH):
+			successfulVend = True
+		else:
+			successfulVend = False
+
+		return successfulVend
+
+
+	def restart():
+		"""
+		Restarts JUST the Raspberry Pi, no other vending machine hardware
+		Use this only if Zoom or GUI has bug that can't fixed
+		"""
+
+		check_call("sudo reboot", shell=True)
+
+
+	def startZoom():
+		"""
+		Use this start Zoom app, full screen it, and then pyautogui click to start meeting
+		https://support.zoom.us/hc/en-us/articles/205683899-hot-keys-and-keyboard-for-zoom
+		https://superuser.com/questions/1563255/start-a-zoom-meeting-from-the-command-line
+		"""
+
+		# Start Zoom application and minimize to give ordering GUI screen
+		command =  "xdg-open " + MISSION_CONTROL_ZOOM_URL
+		check_call(command, shell=True)
+
+
+		#TODO FIND BUTTON X Y TO JOIN MEEETING
+		time.pause(3)
+		X_POS = 500
+		Y_POS = 700
+		pyautogui.click(XPOS, YPOS)
+		time.pause(3)
+
+		# Enter fullscreen after meeting has started
+		pyautogui.press('esc')
+
+		#TODO FIND BUTTON X Y TO TURN ON CAMERA IF NOT TODO DEFAULT ON
+		time.pause(3)
+		X_POS = 200
+		Y_POS = 1820
+		pyautogui.click(XPOS, YPOS)
+		time.pause(3)
+
+		#TODO FIND BUTTON X Y TO TURN ON MIC IF NOT TODO DEFAULT ON
+		time.pause(3)
+		X_POS = 100
+		Y_POS = 1820
+		pyautogui.click(XPOS, YPOS)
+		time.pause(3)
+
+		# Exit fullscreen
+		pyautogui.press('esc')
+
+		# TODO Minimize Zoom app so that Limonada GUI as focus
+		# https://support.zoom.us/hc/en-us/articles/201362103-Minimizing-and-exiting-Zoom?mobile_site=true
+		# To minimize the Zoom desktop client window so that it continues to run in the background, click on the green circle with the x inside located at the top-right corner of the Zoom window. 
+		time.pause(3)
+		X_POS = 1000
+		Y_POS = 80
+		pyautogui.click(XPOS, YPOS)
+		time.pause(3)
+
+
+	def takeWFOVpicture():
+		"""
+		"""
+		img = ComputerVision.ScanQRcode(CAM2)
+
+
+	def scanID():
+		"""
+		"""
+		#Tapmotic - v2020.0/ComputerVision directory?
+		img = ComputerVision.ScanQRcode(CAM2)
+
+
+if __name__ == "_main_":
+
+	# Hardware back-end driver that launches GUI front-end
+	# TODO @Murali - OF DOES GUI front-end driver call into back-end?
+
 	print("CODE STARTS RUNNING HERE")
 
+	# Rotate screen into portrait mode
+	# https://www.raspberrypi.org/forums/viewtopic.php?t=212008
+	check_call("xrandr --output HDMI-1 --primary --mode 1920x1080 --pos 0x0 --rotate right --output DP-1 --off", shell=True)
+
+
+	driverObj = Driver(state='-2')
+	kiosk =  LimonadaStateMachine(driverObj)
+	print("Boot Screen is STATE /#")
+	print(driverObj.state)
+
+	kiosk.Desktop_To_Zoom()
+	print("Zoom Screen is STATE /#")
+	print(driverObj.state)
+
+	kiosk.Zoom_To_AppHomeScreen()
 	lauchGUI(QTPY)
 
 	validState = True
 
 	while(validState):
-		stateMachine()
 
-def unitTest():
-	print("Starting Driver.py Unit Test")
+		nextState = nextStateIs(GUIdrivenState)
+		if(nextState == '1'):
+			kiosk.currentState = LimonadaStateMachine.qtpyHomeScreen
+			#kiosk.run(qtpyHomeScreen)
+		elif(nextState == '2'):
+			print("TODO")
+		elif(nextState == '3'):
+			print("TODO")
+		elif(nextState == '4'):
+			print("TODO")
+		elif(nextState == '5'):
+			print("TODO")
+		else:
+			print("INVALID STATE")
 
-
-def launchGUI(framework):
-	"""
-    #TODO
-
-    Key arguments:
-   	framework -- INTERGER: Global CONSTANT for GUI framework this code should launch in parallel
-
-    Return:
-    OK if GUI was launched succesfully, and NOT_OK otherwise
-	"""
-
-	if(framework == FLASK):
-		Debug.Dprint("GUI generated using Flask microframework")
-		#TODO Launch GUI.py on 2nd Pi or 2nd thread
-		return OK
-	elif(framework == QTPY):
-		Debug.Dprint("GUI generated using QTPY framework")
-		#TODO Launch GUI.py on 2nd Pi or 2nd thread
-		return OK
-	else:
-		Debug.Dprint("LIKE A DIFFERENT GUI BUILDER? SUBMIT PULL REQUEST")
-
-	return NOT_OK
-
-
-def vend(productID):
-	"""
-    #TODO
-
-    Key argument(s):
-   	productID -- INTERGER: Unique ID for each type on Limonada can flavor
-
-    Return:
-    True if ALL the steps to vend a can where successfull; False otherwise
-	"""
-	successfulVend = False
-
-	if(productID == CANNED_DRINK):
-		vendPin = SHIELD_PIN_1")
-	elif(productID == GIFT_CARD_IN_CAN):
-		vendPin = SHIELD_PIN_2")
-	else:
-		DebugObject = Debug(DEBUG_STATEMENTS_ON, THIS_CODES_FILENAME)
-		DebugObject.Dprint("INVALID productID passed as parameter to Driver.vend()")
-
-	GPIO.setup(vendPin, GPIO.OUT, initial=GPIO.LOW)
-	GPIO.output(vendPin, HIGH)
-	time.pause(VEND_DELAY_IN_SEC)
-	GPIO.output(vendPin, LOW)
-
-	GPIO.setup(sensePin, GPIO.out, initial=GPIO.LOW)
-	itemSensed = GPIO.input(sensePin)
-	time.pause(SENSE_DELAY_IN_SEC)
-
-	if(itemSensed = HIGH):
-		successfulVend = True
-	else:
-		successfulVend = False
-
-	return successfulVend
-
-
-def restart():
-	"""
-	Restarts JUST the Raspberry Pi, no other vending machine hardware
-	Use this only if Zoom or GUI has bug that can't fixed
-	"""
-
-	check_call("sudo reboot", shell=True)
-
-
-def startZoom():
-	"""
-	Use this start Zoom app, full screen it, and then pyautogui click to start meeting
-	"""
-	# https://superuser.com/questions/1563255/start-a-zoom-meeting-from-the-command-line
-	#TODO REMOVE check_call("sudo zoom", shell=True)
-
-	# Start Zoom application and minimize to give ordering GUI screen
-	command =  "xdg-open " + MISSION_CONTROL_ZOOM_URL
-	check_call(command, shell=True)
-	pyautogui.press('f10') 		# Fullscreen so mouse can click "Join Meeting" button
-
-	#TODO FIND BUTTON LOCATIO
-	time.pause(3)
-	X_POS = 500
-	Y_POS = 700
-	pyautogui.click(XPOS, YPOS)
-	time.pause(3)
-
-
-def takeWFOVpicture():
-	"""
-	"""
-	img = ComputerVision.ScanQRcode(CAM2)
-
-
-def scanID():
-	"""
-	"""
-	#Tapmotic - v2020.0/ComputerVision directory?
-	img = ComputerVision.ScanQRcode(CAM2)
-
-
-def stateMachine(nextState):
-	"""
-	Control for branching Limonada state machine defined at:
-	https://lucid.app/lucidchart/a8d9b1e9-b963-4709-b8cf-a47e7ee67a6e/edit?beaconFlowId=3D4DF78ED9E6E012&page=0_0#
-
-	TODO pip install python-statemachine
-
-    Key arguments:
-   	currentState -- INTERGER: Current state that GUI is in
-    nextState -- INTERGER:  State that GUI would like driver.py to move to
-
-    Return:
-    Next state GUI should move to
-    """
-
-    
-
+		# Pause 10 ms to give CPU extra time in while loop
+		time.pause(0.010)
--- LimonadaStateMachine.py
+++ LimonadaStateMachine.py
@@ -8,16 +8,36 @@ __date__    =  "Late Updated: 2021-06-14"
 __doc__     =  "Process flow state machine for hardware & GUI syncing"
 """
 
-# Use a state machine to sync the process flow betwwen hardware and GUI
+# This state machine allow syncing the process flow between hardware and GUI
 # https://pypi.org/project/python-statemachine/
 from statemachine import StateMachine, State
 
 class LimonadaStateMachine(StateMachine):
 
-	bootScreen = State('-1', initial=True)
+	# Lucid diagram for Limonada State Machine
+	# https://lucid.app/lucidchart/a8d9b1e9-b963-4709-b8cf-a47e7ee67a6e/edit?beaconFlowId=3D4DF78ED9E6E012&page=0_0#
+
+	# Custom portrait mode boot (aka splash) screen
+	# https://www.raspberrypi.org/forums/viewtopic.php?t=212008
+	# https://keytosmart.com/single-board-computers/raspberry-pi/custom-splash-screen-for-raspberry-pi-raspbian/
+	piBootScreen = State('-2', initial=True)
+
+	# Standard Pi Desktop in portrait mode
+	# https://www.raspberrypi.org/forums/viewtopic.php?t=224229
+	piDesktopScreen = State('-1')
+
+	# Full Screen Zoom application
+	# TODO Should this be in portrait mode also?
 	zoomScreen = State('0')
-	homeScreen = State('1')
+
+	# First QTPY GUI screen
+	qtpyHomeScreen = State('1')
 	voiceBubbleScreen = State('2')
 	buttonScreen = State('3')
 	vendScreen = State('4')
 	endScreen = State('5')
+
+
+	Desktop_To_Zoom = piDesktopScreen.to(zoomScreen)
+	Zoom_To_AppHomeScreen = zoomScreen.to(qtpyHomeScreen)
+
