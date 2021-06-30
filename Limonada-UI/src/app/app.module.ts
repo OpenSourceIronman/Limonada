38e000f935bc98c1dbe64f6328f4de141d1eca8c
Muralikrishna  Dulla <muralikrishnadulla@muralikshnasmbp.myfiosgateway.com>
2021-06-30 14:40:43 -0400

    Angular POC

--- Limonada-UI/src/app/app.module.ts
+++ Limonada-UI/src/app/app.module.ts
@@ -1,22 +1,42 @@
-import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { FormsModule, ReactiveFormsModule } from "@angular/forms";
+import { HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http';
+import { RouterModule } from '@angular/router';
 
 import { AppComponent } from './app.component';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { NavMenuComponent } from './nav-menu/nav-menu.component';
 import { HomeComponent } from './home/home.component';
-import {MatCardModule} from "@angular/material/card";
-import {MatStepperModule} from "@angular/material/stepper";
+import { CounterComponent } from './counter/counter.component';
+import { FetchDataComponent } from './fetch-data/fetch-data.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatDatepickerModule, MatInputModule, MatNativeDateModule, MatFormFieldModule } from '@angular/material';
+import { MatIconModule } from '@angular/material/icon';
+import { MatCardModule } from '@angular/material/card';
+import {MatButtonModule} from '@angular/material/button';
+import { SecondComponentComponent } from './second-component/second-component.component';
+
+
 
 @NgModule({
   declarations: [
     AppComponent,
-    HomeComponent
+    NavMenuComponent,
+    HomeComponent,
+    SecondComponentComponent,
+    CounterComponent,
+    FetchDataComponent
   ],
   imports: [
-    BrowserModule,
-    BrowserAnimationsModule,
-    MatCardModule,
-    MatStepperModule,
+    BrowserModule.withServerTransition({ appId: 'ng-cli-universal' }),
+    HttpClientModule, ReactiveFormsModule, MatCardModule,MatButtonModule,
+    FormsModule, MatDatepickerModule, MatFormFieldModule, MatInputModule, MatNativeDateModule, MatIconModule,
+    RouterModule.forRoot([
+      { path: '', component: HomeComponent, pathMatch: 'full' },
+      { path: 'counter', component: CounterComponent },
+      { path: 'second', component: SecondComponentComponent },
+    ]),
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- Limonada-UI/src/app/home/home.component.ts
+++ Limonada-UI/src/app/home/home.component.ts
@@ -1,32 +1,47 @@
-import {Component, OnInit} from '@angular/core';
-import {FormControl, ValidationErrors} from "@angular/forms";
-import {NgxAgeValidator} from "ngx-age-validator";
-import {MatDatepickerModule} from '@angular/material/datepicker';
-import {MatError} from "@angular/material/form-field";
+import { Component } from '@angular/core';
+import { FormBuilder, FormControl, FormGroup } from '@angular/forms';
+import { MatDatepickerInputEvent } from '@angular/material';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.css']
 })
-export class HomeComponent implements OnInit {
-
-
-  ageFormControl = new FormControl();
-
-  ngOnInit(): void {
-    this.ageFormControl = new FormControl(null, [NgxAgeValidator(18, 40)])
-
-    this.ageFormControl.valueChanges.subscribe(() => {
-
-      const controlErrors: ValidationErrors | null = this.ageFormControl.errors;
-      if (controlErrors != null) {
-        Object.keys(controlErrors).forEach(keyError => {
-          console.log(' keyError: ' + keyError + ', err value: ', controlErrors[keyError]);
-        });
-      }
-
-    })
+export class HomeComponent {
+  date = new FormControl(new Date());
+  value: Date = new Date();
+  getYear : any;
+  checkDate : Date;
+  form : FormGroup;
+  isShow : boolean;
+  constructor(private fb: FormBuilder , private router:Router ) {
+    this.form = this.fb.group({
+      format1: new FormControl(new Date()),
+      format2: new FormControl(new Date())
+    });
   }
+  onChangeEvent(event: any) {
+    debugger
+    var data = event.target.value;
+    if (data >= 18) {
+      this.isShow = true;
+    }
+    else {
+      this.isShow = false;
+    }
+    return this.isShow;
+  }
+  btnClick= function () {
+    if(this.isShow==true){
+      this.router.navigateByUrl('/second');
+    }
 
+};
+  // addEvent(type: string, event: MatDatepickerInputEvent<Date>) {
+  //   debugger
+  //   this.checkDate = event.target.value;
+  //   this.getYear = this.checkDate;
+
+  // }
 }
