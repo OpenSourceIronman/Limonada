0aacaaa223cbee14a152d44212f016db93b93401
Blaze Sanders <blaze.d.a.sanders@gmail.com>
2021-07-08 09:17:17 -0700

    import errors

--- Limonada-Backend/switch.py
+++ Limonada-Backend/switch.py
@@ -1,4 +1,4 @@
-# Allow Adafruit FT232H to communicate over USB 
+# Allow Adafruit FT232H to communicate over USB
 import board
 import digitalio
 import os
@@ -17,43 +17,33 @@ def toggle():
 
 	hdmiNPN.value = False
 	time.sleep(1.5)
-        hdmiNPN.value = True
+	hdmiNPN.value = True
 
 
 if __name__ == "__main__":
-	# Run the following commands in the command line before running$	
+	# Run the following commands in the command line before running
 	# export BLINKA_FT232H=1
-	check_call("export BLINKA_FT232H=1", shell=True")
+	check_call("export BLINKA_FT232H=1", shell=True)
 
 	# python3
-                check_call("python3", shell=True)
+	check_call("python3", shell=True)
 
-                # import board
-                pyautogui.write("import board")
+	# import board
+	pyautogui.write("import board")
 
-                # from pyftdi.ftdi import Ftdi
-                pyautogui.write("from pyftdi.ftdi import Ftdi", shell=True)
+	# from pyftdi.ftdi import Ftdi
+	pyautogui.write("from pyftdi.ftdi import Ftdi", shell=True)
 
-                # Ftdi().open_from_url('ftdi:///?')
-                pyautogui.write("Ftdi().open_from_url('ftdi:///?')")
+	# Ftdi().open_from_url('ftdi:///?')
+	pyautogui.write("Ftdi().open_from_url('ftdi:///?')")
 
- 		# python3
-                check_call("python3", shell=True)
+	# python3
+	check_call("python3", shell=True)
 
-                # import os
-                pyautogui.write("import os")
+	# import os
+	pyautogui.write("import os")
 
-                # os.environ["BLINKA_FT232H"]
-                # import digitialio
+	command = 'os.environ["BLINKA_FT232H"]'
+	pyautogui.write(command)
 
-                successfullSwitch = False
-                led = digitalio.DigitalInOut(board.D7)
-                led = led.direction = digitalio.Direction.OUTPUT
-
-                led.value = False
-                time.sleep(1.5)
-                led.value = True
-
-                successfullSwitch = True
-
-                return successfullSwitch
+	pyautogui.write("import digitialio")
